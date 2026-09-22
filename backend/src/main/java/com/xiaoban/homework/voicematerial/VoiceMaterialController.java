a443fd945ec9225f336e88f3a998e7fef44df6d3
gyguan <guangangyu@163.com>
2026-09-22 17:21:40 +0800

    fix: 补充素材读取响应类型导入

--- backend/src/main/java/com/xiaoban/homework/voicematerial/VoiceMaterialController.java
+++ backend/src/main/java/com/xiaoban/homework/voicematerial/VoiceMaterialController.java
@@ -11,6 +11,7 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
