cab48bd5b0f7bca82c27820d84b07baad44af869
gyguan <guangangyu@163.com>
2026-09-22 12:11:07 +0800

    feat(voice): add shared media and material persistence for #298

---
+++ backend/src/main/java/com/xiaoban/homework/voicematerial/VoiceMaterialBatchRepository.java
@@ -0,0 +1,6 @@
+package com.xiaoban.homework.voicematerial;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoiceMaterialBatchRepository extends JpaRepository<VoiceMaterialBatchEntity, UUID> {}
